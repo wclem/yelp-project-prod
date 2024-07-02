d1e4537b9fb479057dc06806454b4dd846bc1a65
William Clemens <wclem2008@gmail.com>
2024-07-02 12:12:02 -0700

    Streamlined product python file, added timing

--- yelp_proj.py
+++ yelp_proj.py
@@ -8,65 +8,68 @@ import re
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
 import joblib
+import time
 
+def load_data():
+    start = time.time()
+    cnx = sqlite3.connect(r'rev_db3.db')
+    curs = cnx.cursor()
+    df = pd.read_sql_query("SELECT * FROM data", cnx)
+    end = time.time()
+    print(f"Loading time: {end - start} sec.")
+    return df
 
-# Create the connection
-cnx = sqlite3.connect(r'rev_db3.db')
-curs = cnx.cursor()
+def clean_text(input_df):
+    custom_stopwords = {'don', "don't", 'ain', "aren't", 'couldn', "couldn't",
+                    'didn', "didn't", 'doesn', "doesn't", 'hadn', "hadn't",
+                    'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma',
+                    'mightn', "mightn't", 'mustn', "musn't", 'needn', "needn't", 
+                    'shan', "shan't", 'no', 'nor', 'not', 'shouldn', "shouldn't",
+                    'wasn', "wasn't", 'weren', "weren't", 'won', "won't", 'wouldn', "wouldn't"}
 
+    corpus = []
+    ps = PorterStemmer()
+    stop_words = set(stopwords.words("english")) - custom_stopwords
+    text = input_df["text"]
 
-# create the dataframe from a query
-df = pd.read_sql_query("SELECT * FROM data", cnx)
-curs.execute("SELECT COUNT(*) FROM data").fetchall()
+    for i in range(len(text)):
+        review = re.sub('[^a-zA-Z]', ' ',text[i])
+        review = review.lower()
+        review = review.split()
+        review = [ps.stem(word) for word in review if word not in stop_words]
+        review = " ".join(review)
+        corpus.append(review)
+    return corpus
 
+def vectorize(text):
+    start = time.time()
+    vectorizer.fit(text)
+    x = vectorizer.transform(text)
+    end = time.time()
+    print(f"Vectorizing time: {end - start} sec.")
+    return x.toarray()
 
+def train_model(x, y):
+    start = time.time()
+    nb = MultinomialNB()
+    nb.fit(x, y)
+    print("Model trained")
+    end = time.time()
+    print(f"Model training time: {end - start} sec.")
+    return nb
+    
+def dump_pickles(nb, vectorizer):
+    joblib.dump(nb, "yelpmodel.pkl")
+    joblib.dump(vectorizer, "yelp_vectorizer.pkl")
 
-vectorizer = CountVectorizer(min_df=1)
-text = df["text"]
-
-
-custom_stopwords = {'don', "don't", 'ain', "aren't", 'couldn', "couldn't",
-                   'didn', "didn't", 'doesn', "doesn't", 'hadn', "hadn't",
-                   'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma',
-                   'mightn', "mightn't", 'mustn', "musn't", 'needn', "needn't", 
-                   'shan', "shan't", 'no', 'nor', 'not', 'shouldn', "shouldn't",
-                   'wasn', "wasn't", 'weren', "weren't", 'won', "won't", 'wouldn', "wouldn't"}
-
-corpus = []
-ps = PorterStemmer()
-stop_words = set(stopwords.words("english")) - custom_stopwords
-
-for i in range(100):
-    review = re.sub('[^a-zA-Z]', ' ',text[i])
-    review = review.lower()
-    review = review.split()
-    review = [ps.stem(word) for word in review if word not in stop_words]
-    review = " ".join(review)
-    corpus.append(review)
-
-text = text.head(10000)
-
-# call `fit` to build the vocabulary
-vectorizer.fit(text)
-
-# call `transform` to convert text to a bag of words
-x = vectorizer.transform(text)
-
-# CountVectorizer uses a sparse array to save memory, but it's easier in this assignment to 
-# convert back to a "normal" numpy array
-x = x.toarray()
-
-y=df["stars"].head(10000)
-
-
-X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.20, random_state=42)
-
-nb = MultinomialNB()
-nb.fit(X_train, y_train)
-y_pred = nb.predict(X_test)
-
-
-
-joblib.dump(nb, "yelpmodel.pkl")
-
-joblib.dump(vectorizer, "yelp_vectorizer.pkl")
+if __name__ == "__main__":
+    print('Starting test')
+    vectorizer = CountVectorizer(min_df=1)
+    data = load_data()
+    data = data.head(10000) #limiting to 10000 records for this project
+    text = clean_text(data)
+    X = vectorize(text)
+    y=data["stars"].head(10000)
+    trained_model = train_model(X, y)
+    dump_pickles(trained_model, vectorizer)
+    
